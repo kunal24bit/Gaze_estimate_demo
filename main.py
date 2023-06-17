29b071abeb25ad02143d3e38b88db373ff3dc021
Kunal Verma <64734142+kunal24bit@users.noreply.github.com>
2023-06-17 11:31:38 +0530

    streamline main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-
-from typing import Union, Tuple
 import cv2
-from fastapi import FastAPI, File, Request
-from fastapi.param_functions import Form
+import numpy as np
+import streamlit as st
 from iris_tracking import process_frame
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
-templates = Jinja2Templates(directory = "templates")
 
 
-@app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def main():
+    st.title("Iris Tracking")
 
+    threshold = st.number_input("Threshold", min_value=0.0, max_value=1.0, step=0.01)
 
-@app.post("/track_iris")
-async def track_iris(request: Request, threshold: float = Form(...)) -> Tuple[str, float]:
     cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        st.error("Failed to open the webcam.")
+        return
+
     while True:
         ret, frame = cap.read()
 
@@ -27,18 +22,19 @@ async def track_iris(request: Request, threshold: float = Form(...)) -> Tuple[st
 
         iris_pos, ratio = process_frame(frame, threshold)
 
-        cv2.putText(frame,"Iris_pos and ratio are {}, {}".format(iris_pos, ratio),(30, 30),cv2.FONT_HERSHEY_PLAIN ,1.2, (255,0, 0), 1, cv2.LINE_AA)
+        frame_with_info = np.copy(frame)
+        cv2.putText(frame_with_info, f"Iris position and ratio: {iris_pos}, {ratio}", (30, 30),
+                    cv2.FONT_HERSHEY_PLAIN, 1.2, (255, 0, 0), 1, cv2.LINE_AA)
 
-        cv2.imshow("Iris Tracking", frame)
+        st.image(frame_with_info, channels="BGR")
 
-        if cv2.waitKey(1) ==  ord("q"):
+        if st.button("Stop"):
             break
+
     cap.release()
     cv2.destroyAllWindows()
 
-    return iris_pos, ratio
 
-import uvicorn
-if __name__ =="_main__":
-    uvicorn.run(app, host = "0.0.0.0", port = 8000)
+if __name__ == "__main__":
+    main()
 
